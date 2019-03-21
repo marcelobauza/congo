e75ddfd64c198d2dbeab0d439ab2f68bc73ccc33
Sergio <asergiop21@gmail.com>
2019-03-21 15:29:30 -0300

    Login and script head

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  layout :layout_by_resource
+  
+  
+  def layout_by_resource
+    if devise_controller? and
+        resource_name == :user and
+        action_name == 'new'
+      "login" 
+    else
+      "application"
+    end
+  end
+
+
+
+
 end
