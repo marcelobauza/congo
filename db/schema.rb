4b41b63f3005681d06ca0a84939b13e635010b17
Sergio <asergiop21@gmail.com>
2019-03-27 13:04:55 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,100 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_172830) do
+ActiveRecord::Schema.define(version: 2019_03_27_001941) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
+  enable_extension "postgis"
+  enable_extension "postgis_topology"
+
+  create_table "counties", id: :integer, default: nil, force: :cascade do |t|
+    t.geometry "the_geom", limit: {:srid=>4326, :type=>"multi_polygon"}
+    t.string "name"
+    t.integer "code"
+    t.string "state"
+    t.string "transaction_data"
+    t.string "demography_data"
+    t.string "legislation_data"
+    t.string "sales_project_data"
+    t.string "created_at"
+    t.string "updated_at"
+    t.string "simple_geom"
+    t.string "future_project_data"
+    t.string "commercial_project_data"
+    t.float "rate"
+    t.string "zip_file_file_name"
+    t.string "zip_file_content_type"
+    t.integer "zip_file_file_size"
+    t.string "zip_file_updated_at"
+    t.integer "code_sii"
+    t.integer "number_last_project_future"
+    t.index ["the_geom"], name: "sidx_counties_the_geom", using: :gist
+  end
+
+  create_table "layer_types", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "property_type_id"
+    t.string "address"
+    t.integer "sheet"
+    t.integer "number"
+    t.date "inscription_date"
+    t.string "buyer_name"
+    t.bigint "seller_type_id"
+    t.string "department"
+    t.string "blueprint"
+    t.decimal "uf_value"
+    t.decimal "real_value"
+    t.decimal "calculated_value"
+    t.date "quarter"
+    t.date "quarter_date"
+    t.integer "year"
+    t.decimal "sample_factor", precision: 8, scale: 2
+    t.bigint "county_id"
+    t.geometry "the_geom", limit: {:srid=>4326, :type=>"st_point"}
+    t.integer "cellar", default: 0
+    t.integer "parkingi", default: 0
+    t.string "role"
+    t.string "seller_name"
+    t.string "buyer_rut"
+    t.decimal "uf_m2"
+    t.integer "tome"
+    t.string "lot"
+    t.string "block"
+    t.string "village"
+    t.decimal "surface", precision: 8, scale: 2
+    t.string "requiring_entity"
+    t.string "comments"
+    t.bigint "user_id"
+    t.integer "surveyor_id"
+    t.boolean "active", default: true
+    t.integer "bimester"
+    t.integer "code_sii"
+    t.decimal "total_surface_building", precision: 8, scale: 2
+    t.decimal "total_surface_terrain", precision: 8, scale: 2
+    t.decimal "uf_m2_u", precision: 8, scale: 2
+    t.decimal "uf_m2_t", precision: 8, scale: 2
+    t.string "building_regulation"
+    t.string "role_1"
+    t.string "role_2"
+    t.string "code_destination"
+    t.string "code_material"
+    t.string "year_sii"
+    t.string "role_associated"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["county_id"], name: "index_transactions_on_county_id"
+    t.index ["property_type_id"], name: "index_transactions_on_property_type_id"
+    t.index ["seller_type_id"], name: "index_transactions_on_seller_type_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
