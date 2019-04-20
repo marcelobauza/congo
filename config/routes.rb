25f89653ea4c20e9e0e93f6a766e73d35f301d97
Sergio <asergiop21@gmail.com>
2019-04-20 10:19:58 -0300

    add routes layer_types

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :counties
   resources :property_types
   resources :transactions
+  resources :layer_types
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'transactions#dashboards'
