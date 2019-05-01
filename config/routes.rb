cc65973566e7a7fd3022756c46bbad96fd1c3aac
Sergio <asergiop21@gmail.com>
2019-05-01 05:31:12 -0300

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,20 @@ Rails.application.routes.draw do
 
 
 
+  resources :project_mixes
+  resources :project_statuses
+  resources :project_instance_mixes
+  resources :project_instances
+  resources :projects
   get 'dashboards/index'
   get 'counties/find' => 'counties#find'
   get 'transactions/transactions_summary' => 'transactions#transactions_summary'
-  get 'transactions/dashboards' => 'transactions#dashboards'
+  get 'projects/projects_summary' => 'projects#projects_summary'
   get 'future_projects/future_projects_summary' => 'future_projects#future_projects_summary'
+  
+  get 'transactions/dashboards' => 'transactions#dashboards'
   get 'future_projects/dashboards' => 'future_projects#dashboards'
+  get 'projects/dashboards' => 'projects#dashboards'
   
   namespace :admin do
       resources :periods
@@ -19,7 +27,8 @@ Rails.application.routes.draw do
   resources :future_projects
   resources :future_project_types
   resources :project_types
+  resources :seller_types
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'transactions#dashboards'
+  root 'dashboards#index'
 end
