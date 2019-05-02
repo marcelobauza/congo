5f86b0bbed9572c3430853de7541d18e2c9dff0a
Sergio <asergiop21@gmail.com>
2019-05-02 12:50:16 -0300

    add config conunties

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,14 +1,31 @@
 Congo.namespace('dashboards.action_index');
 
-Congo.dashboards.action_index = function(){
+Congo.dashboards.config= {
+  county_name: '',
+  county_id: '',
+  layer_type: 'transactions_info'
+}
 
+Congo.dashboards.action_index = function(){
   init= function(){
-  
-           Congo.map_utils.init();  
-  
-  
+    Congo.map_utils.init();  
   }
-return {
-      init: init,
-    }
+
+  /*  indicators = function(){
+
+    $.ajax({
+      type: 'GET',
+      url: '/transactions/transactions_summary.json',
+      datatype: 'json',
+      data: {to_year:"2018", locale:"es", periods_quantity: "5", to_period: "6", county_id:"52" },
+      success: function(data){
+        console.log(data);
+      }
+    })
+  }
+*/
+  return {
+    init: init,
+  //  indicators: indicators
+  }
 }();
