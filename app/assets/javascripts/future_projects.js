3e11841a301f6db3f70cd9245aac25487df91f0f
jvillegas88 <villegas.jonathan88@gmail.com>
2019-05-13 20:02:00 -0300

    Separates the div of Informacion General

--- app/assets/javascripts/future_projects.js
+++ app/assets/javascripts/future_projects.js
@@ -33,206 +33,221 @@ Congo.future_projects.action_dashboards = function(){
           var title = reg['title'];
           var series = reg['series'];
 
-          var datasets = [];
+          //Extraemos los datos de "Informacion General" para tratarlos por separado
+          if (title == "Informacion General") {
 
-          // Extraemos las series
-          $.each(series, function(a, b){
+            var info = reg['data'];
 
-            var label = b['label']
-            var data = b['data']
+            // Creamos y adjuntamos el div contenedor
+            var chart_container = document.createElement('div');
+            chart_container.className = 'chart-container'+i+' card';
+            $('.overlay').append(chart_container);
 
-            if (label == 'ANTEPROYECTO') {
-              serie_colour = '#60c843'
-            }
-            if (label == 'PERMISO DE EDIFICACION') {
-              serie_colour = '#0f115b'
-            }
-            if (label == 'RECEPCION MUNICIPAL') {
-              serie_colour = '#eb2817'
-            }
-            if (label == 'Tasa Permiso / Anteproyecto') {
-              serie_colour = '#0f115b'
-            }
-            if (label == 'Tasa Recepciones / Permisos') {
-              serie_colour = '#eb2817'
-            }
-
-            var name = [];
-            var count = [];
-
-            // Extraemos los datos de las series
-            $.each(data, function(c, d){
-              name.push(d['name'])
-              count.push(d['count'])
+            // Extraemos los datos y los adjuntamos al div contenedor
+            $.each(info, function(y, z){
+              name = z['name'];
+              label = z['count']
+              item = name+': '+label+'<br>';
+              $('.chart-container'+i).append(item);
             })
 
-            // Guardamos "datasets" y "chart_type"
-            if (title == 'Informacion General') {
-              chart_type = 'horizontalBar';
-              datasets.push({
-                label: label,
-                data: count,
-              })
-            }
+          // Extraemos y publicamos los gráficos
+          } else {
 
-            if (title == 'Tipo de Expendiente') {
-              chart_type = 'pie';
-              cantidad = count.length;
-              rancolor = randomColor({
-                count: cantidad,
-                format: 'rgb',
-                seed: 1,
-              })
-              datasets.push({
-                label: label,
-                data: count,
-                backgroundColor: rancolor,
-              })
-            }
+            var datasets = [];
 
-            if (title == 'Tipos de destino') {
-              chart_type = 'pie';
-              cantidad = count.length;
-              rancolor = randomColor({
-                count: cantidad,
-                format: 'rgb',
-                seed: 1,
-              })
-              datasets.push({
-                label: label,
-                data: count,
-                backgroundColor: rancolor,
-              })
-            }
+            // Extraemos las series
+            $.each(series, function(a, b){
 
+              var label = b['label']
+              var data = b['data']
+
+              if (label == 'ANTEPROYECTO') {
+                serie_colour = '#60c843'
+              }
+              if (label == 'PERMISO DE EDIFICACION') {
+                serie_colour = '#0f115b'
+              }
+              if (label == 'RECEPCION MUNICIPAL') {
+                serie_colour = '#eb2817'
+              }
+              if (label == 'Tasa Permiso / Anteproyecto') {
+                serie_colour = '#0f115b'
+              }
+              if (label == 'Tasa Recepciones / Permisos') {
+                serie_colour = '#eb2817'
+              }
+
+              var name = [];
+              var count = [];
+
+              // Extraemos los datos de las series
+              $.each(data, function(c, d){
+                name.push(d['name'])
+                count.push(d['count'])
+              })
+
+
+              // Guardamos "datasets" y "chart_type"
+              if (title == 'Tipo de Expendiente') {
+                chart_type = 'pie';
+                cantidad = count.length;
+                rancolor = randomColor({
+                  count: cantidad,
+                  format: 'rgb',
+                  seed: 1,
+                })
+                datasets.push({
+                  label: label,
+                  data: count,
+                  backgroundColor: rancolor,
+                })
+              }
+
+              if (title == 'Tipos de destino') {
+                chart_type = 'pie';
+                cantidad = count.length;
+                rancolor = randomColor({
+                  count: cantidad,
+                  format: 'rgb',
+                  seed: 1,
+                })
+                datasets.push({
+                  label: label,
+                  data: count,
+                  backgroundColor: rancolor,
+                })
+              }
+
+              if (title == 'Tipo de Destino') {
+                chart_type = 'bar';
+                datasets.push({
+                  label: label,
+                  data: count,
+                  backgroundColor: serie_colour,
+                })
+              }
+
+              if (title == 'Cantidad de unidades nuevas / bimestre') {
+                chart_type = 'line';
+                datasets.push({
+                  label: label,
+                  data: count,
+                  borderColor: serie_colour,
+                  lineTension: 0,
+                  pointRadius: 0,
+                  borderWidth: 3,
+                  fill: false,
+                })
+              }
+
+              if (title == 'Superficie edificada por expediente') {
+                chart_type = 'line';
+                datasets.push({
+                  label: label,
+                  data: count,
+                  borderColor: serie_colour,
+                  lineTension: 0,
+                  pointRadius: 0,
+                  borderWidth: 3,
+                  fill: false,
+                })
+              }
+
+              if (title == 'Tasas') {
+                chart_type = 'line';
+                datasets.push({
+                  label: label,
+                  data: count,
+                  borderColor: serie_colour,
+                  lineTension: 0,
+                  pointRadius: 0,
+                  borderWidth: 3,
+                  fill: false,
+                })
+              }
+
+              chart_data = {
+                labels: name,
+                datasets: datasets
+              }
+
+            })
+
+            // Guardamos "options"
             if (title == 'Tipo de Destino') {
-              chart_type = 'bar';
-              datasets.push({
-                label: label,
-                data: count,
-                backgroundColor: serie_colour,
-              })
-            }
-
-            if (title == 'Cantidad de unidades nuevas / bimestre') {
-              chart_type = 'line';
-              datasets.push({
-                label: label,
-                data: count,
-                borderColor: serie_colour,
-                lineTension: 0,
-                pointRadius: 0,
-                borderWidth: 3,
-                fill: false,
-              })
-            }
-
-            if (title == 'Superficie edificada por expediente') {
-              chart_type = 'line';
-              datasets.push({
-                label: label,
-                data: count,
-                borderColor: serie_colour,
-                lineTension: 0,
-                pointRadius: 0,
-                borderWidth: 3,
-                fill: false,
-              })
-            }
-
-            if (title == 'Tasas') {
-              chart_type = 'line';
-              datasets.push({
-                label: label,
-                data: count,
-                borderColor: serie_colour,
-                lineTension: 0,
-                pointRadius: 0,
-                borderWidth: 3,
-                fill: false,
-              })
-            }
-
-            chart_data = {
-              labels: name,
-              datasets: datasets
-            }
-
-          })
-
-          // Guardamos "options"
-          if (title == 'Tipo de Destino') {
-            var chart_options = {
-              responsive: true,
-              title: {
-                display: true,
-                text: title,
-                fontSize:15
-              },
-              legend: {
-                display: false,
-              },
-              plugins: {
-                datalabels: {
+              var chart_options = {
+                responsive: true,
+                title: {
+                  display: true,
+                  text: title,
+                  fontSize:15
+                },
+                legend: {
                   display: false,
                 },
-              },
-              scales: {
-                xAxes: [{
-                  stacked: true,
-                }],
-                yAxes: [{
-                  stacked: true,
-                }],
+                plugins: {
+                  datalabels: {
+                    display: false,
+                  },
+                },
+                scales: {
+                  xAxes: [{
+                    stacked: true,
+                  }],
+                  yAxes: [{
+                    stacked: true,
+                  }],
+                }
+              }
+            } else {
+              var chart_options = {
+                responsive: true,
+                title: {
+                  display: true,
+                  text: title,
+                  fontSize:15
+                },
+                legend: {
+                  display: false,
+                },
+                plugins: {
+                  datalabels: {
+                    display: false,
+                  },
+                },
               }
             }
-          } else {
-            var chart_options = {
-              responsive: true,
-              title: {
-                display: true,
-                text: title,
-                fontSize:15
-              },
-              legend: {
-                display: false,
-              },
-              plugins: {
-                datalabels: {
-                  display: false,
-                },
-              },
+
+            var chart_settings = {
+              type: chart_type,
+              data: chart_data,
+              options: chart_options
             }
-          }
 
-          var chart_settings = {
-            type: chart_type,
-            data: chart_data,
-            options: chart_options
-          }
+            // TODO: Adjuntar las clases card-header y card-body
 
-          // TODO: Adjuntar las clases card-header y card-body
+            // Creamos el div contenedor
+            var chart_container = document.createElement('div');
+            chart_container.className = 'chart-container'+i+' card';
 
-          // Creamos el div contenedor
-          var chart_container = document.createElement('div');
-          chart_container.className = 'chart-container'+i+' card';
+            // Creamos el canvas
+            var canvas = document.createElement('canvas');
+            var canvas_id = 'canvas'+i;
+            canvas.id = canvas_id;
 
-          // Creamos el canvas
-          var canvas = document.createElement('canvas');
-          var canvas_id = 'canvas'+i;
-          canvas.id = canvas_id;
+            // Adjuntamos los elementos
+            $('.overlay').append(chart_container);
+            $('.chart-container'+i).append(canvas);
 
-          // Adjuntamos los elementos
-          $('.overlay').append(chart_container);
-          $('.chart-container'+i).append(canvas);
+            var chart_canvas = document.getElementById('canvas'+i).getContext('2d');
+            var final_chart = new Chart(chart_canvas, chart_settings);
 
-          var chart_canvas = document.getElementById('canvas'+i).getContext('2d');
-          var final_chart = new Chart(chart_canvas, chart_settings);
-
-        } // cierra for
-      } // cierra success
-    }) // cierra ajax
-  } // cierra indicator_future_projects
+          } // Cierra if
+        } // Cierra for
+      } // Cierra success
+    }) // Cierra ajax
+  } // Cierra indicator_future_projects
 
   return {
     init: init,
