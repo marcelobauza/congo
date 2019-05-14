5942be805f854b6a4762a794c487091442269ea2
Sergio <asergiop21@gmail.com>
2019-05-13 23:43:17 -0300

    add javascript leaflet-search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require leaflet/leaflet-search
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
--- app/assets/javascripts/map_utils.js
+++ app/assets/javascripts/map_utils.js
@@ -29,6 +29,18 @@ Congo.map_utils = function(){
       layers: [streets]
     }) ;
 
+   map.addControl( new L.Control.Search({
+    url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+    jsonpParam: 'json_callback',
+    propertyName: 'display_name',
+    markerLocation: true,
+    propertyLoc: ['lat','lon'],
+    marker: L.circleMarker([0,0],{radius:30}),
+    autoCollapse: true,
+    autoType: false,
+    minLength: 2
+  }) );
+
     $('#select_circle').on('click', function(event) {
       checked = $('#select_circle').hasClass('active');
       let typeGeometry='circle';
