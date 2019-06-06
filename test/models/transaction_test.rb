f8eecda3087b68bae4fe6edf6ac91b5ab510c206
Sergio <asergiop21@gmail.com>
2019-06-06 17:42:48 -0300

    bug  variable name test

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   #test "group_transaction_by_uf" do
+   #  @filters = {"to_year"=>"2018", "locale"=>"es", "periods_quantity"=>"5", "to_period"=>"6", "county_id"=>"52"} 
+   #   @result = Transaction.group_transactions_by_uf(@filters)
+
+    # assert true
+   #end
 end
