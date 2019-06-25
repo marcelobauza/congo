451107a15e470554b7a695b1c67e2776bfde4524
Sergio <asergiop21@gmail.com>
2019-06-25 17:29:53 -0300

    add county_name in point

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,7 +10,7 @@ class DashboardsController < ApplicationController
   
   def filter_county_for_lon_lat
     @county= County.find_by_lon_lat(params[:lon], params[:lat])
-    render json: {county_id: @county.id }
+    render json: {county_id: @county.id, county_name: @county.name }
   end
 
   def filter_period
