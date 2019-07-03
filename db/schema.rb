ade3294bc70fac9255c6db9876826544b2a912ea
jvillegas88 <villegas.jonathan88@gmail.com>
2019-07-03 19:55:24 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_061605) do
+ActiveRecord::Schema.define(version: 2019_06_28_172644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -410,6 +410,12 @@ ActiveRecord::Schema.define(version: 2019_06_27_061605) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "regions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.boolean "read_only"
@@ -506,6 +512,8 @@ ActiveRecord::Schema.define(version: 2019_06_27_061605) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "region_id"
+    t.index ["region_id"], name: "index_users_on_region_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
@@ -525,6 +533,7 @@ ActiveRecord::Schema.define(version: 2019_06_27_061605) do
   add_foreign_key "import_processes", "users"
   add_foreign_key "pois", "poi_subcategories"
   add_foreign_key "project_instances", "project_statuses"
+  add_foreign_key "users", "regions"
 
   create_function :cleangeometry, sql_definition: <<-SQL
       CREATE OR REPLACE FUNCTION public.cleangeometry(geometry)
