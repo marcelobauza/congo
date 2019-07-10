bb72553c86b01eafd3c2506939c228a9a21c99c1
Sergio <asergiop21@gmail.com>
2019-07-10 01:18:30 -0300

    add module account of user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:account, :update]
+  def account
+    respond_to do |format|
+      format.js 
+    end
+  end
+
+  def update
+
+    @errorMessage = []
+
+    respond_to do |format|
+     if @user.update(user_params)
+      format.js 
+      else
+      format.js
+      @user.errors.any?
+      @user.errors.each do |key, value|
+        @errorMessage.push(value)
+      end
+    end
+    end
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(current_user.id)
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:complete_name, :password, :password_confirmation, :name, :email, :company, :city)
+    end
+
+end
