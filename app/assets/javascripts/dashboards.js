1cefd1c96f07f570c407880723475b9834979101
jvillegas88 <villegas.jonathan88@gmail.com>
2019-07-31 16:57:47 -0300

    Beutify dashboard.js code

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,7 +1,7 @@
 Congo.namespace('dashboards.action_index');
 Congo.namespace('dashboards.action_graduated_points');
 
-Congo.dashboards.config= {
+Congo.dashboards.config = {
   county_name: '',
   county_id: '',
   layer_type: 'future_projects_info',
@@ -15,50 +15,57 @@ Congo.dashboards.config= {
   size_box: [],
   typeGeometry: '',
   boost: false,
-  area :0,
+  area: 0,
   draw_active: false,
   slider_period: []
 }
 
-Congo.dashboards.action_index = function(){
-  init= function(){
-  $('#boost').on('click', function(){
+Congo.dashboards.action_index = function() {
+
+  init = function() {
+
+    $('#boost').on('click', function() {
       area = Congo.dashboards.config.area;
       radius = Congo.dashboards.config.radius;
-    if ((area > 0 && area < 3140000) || (radius > 0 && radius < 1000)){
+      if ((area > 0 && area < 3140000) || (radius > 0 && radius < 1000)) {
         Congo.dashboards.config.boost = true;
         Congo.map_utils.counties();
-    }else{
+      } else {
         console.log("es muy grande");
-    }
-  })
-        $.ajax({
-          async: false,
-          type: 'GET',
-          url: '/dashboards/filter_period.json',
-          datatype: 'json',
-          success: function(data){
-            Congo.dashboards.config.year = data['year'];
-            Congo.dashboards.config.bimester = data['bimester'];
-          }
-        });
+      }
+    })
+
+    $.ajax({
+      async: false,
+      type: 'GET',
+      url: '/dashboards/filter_period.json',
+      datatype: 'json',
+      success: function(data) {
+        Congo.dashboards.config.year = data['year'];
+        Congo.dashboards.config.bimester = data['bimester'];
+      }
+    });
+
     Congo.map_utils.init();
-           
-        $.ajax({
-          async: false,
-          type: 'GET',
-          data: {enable: "true"},
-          url: '/admin/periods/active_periods.json',
-          datatype: 'json',
-          success: function(data){
-            $.each(data, function(key, period){
-              $.each(period, function(a, value){
-               Congo.dashboards.config.slider_period.push(value.bimester +"/"+value.year);
-              })
-            })
-          }
-        });
-  }
+
+    $.ajax({
+      async: false,
+      type: 'GET',
+      data: {
+        enable: "true"
+      },
+      url: '/admin/periods/active_periods.json',
+      datatype: 'json',
+      success: function(data) {
+        $.each(data, function(key, period) {
+          $.each(period, function(a, value) {
+            Congo.dashboards.config.slider_period.push(value.bimester + "/" + value.year);
+          })
+        })
+      }
+    });
+
+  } // Cierra init
 
   // Creamos el overlay
   create_overlay_and_filter_card = function() {
@@ -118,7 +125,8 @@ Congo.dashboards.action_index = function(){
         return handle.classList.contains('handle') || handle.parentNode.classList.contains('handle');
       }
     });
-  };
+
+  } // Cierra create_overlay_and_filter_card
 
   add_county_filter_item = function() {
     $('#item-comuna').remove();
@@ -126,7 +134,7 @@ Congo.dashboards.action_index = function(){
       $('<div>', { // item
         'class': 'text-light bg-secondary px-2 mb-1 py-1 rounded border border-dark shadow',
         'id': 'item-comuna',
-        'text': 'Comuna: '+Congo.dashboards.config.county_name
+        'text': 'Comuna: ' + Congo.dashboards.config.county_name
       })
     )
   }
@@ -144,10 +152,12 @@ Congo.dashboards.action_index = function(){
   }
 }();
 
-Congo.dashboards.action_graduated_points = function(){
-  init=function(){
+Congo.dashboards.action_graduated_points = function() {
+
+  init = function() {}
+
+  return {
+    init: init
   }
-return{
-  init: init
-}
+
 }();
