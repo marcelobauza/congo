cab2fb8d0a0a931aa7d44726c101369c92f5d587
Sergio <asergiop21@gmail.com>
2019-08-09 12:07:16 -0300

    add lots as type layers in dashboards

--- app/assets/javascripts/map_utils.js
+++ app/assets/javascripts/map_utils.js
@@ -426,6 +426,22 @@ Congo.map_utils = function(){
 
     layerControl.addOverlay(sourcePois, "Equipamientos y Servicios");
 
+    //Lots
+    var options_lots = {
+      layers: "inciti_v2:lots",//nombre de la capa (ver get capabilities)
+      format: 'image/png',
+      transparent: 'true',
+      opacity: 1,
+      version: '1.0.0',//wms version (ver get capabilities)
+      tiled: true,
+      styles: 'lots',
+      INFO_FORMAT: 'application/json',
+      format_options: 'callback:getJson',
+      CQL_FILTER: cql_filter_pois
+    };
+    sourceLots = new L.tileLayer.betterWms("http://"+url+":8080/geoserver/wms", options_lots);
+    layerControl.addOverlay(sourceLots, "Predial");
+
     if(county_id != ''){
       var owsrootUrl = 'http://localhost:8080/geoserver/ows';
       var defaultParameters = {
