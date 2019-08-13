c7d143ceeb2ef6258bee70bfd3f6224411aef504
Sergio <asergiop21@gmail.com>
2019-08-13 11:50:53 -0300

    Bug disabled user and include blank in filter of user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -46,7 +46,7 @@ class Admin::UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -73,6 +73,6 @@ class Admin::UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:complete_name, :password, :password_confirmation, :name, :email, :company, :city, :disabled)
     end
 end
