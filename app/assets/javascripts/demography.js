48d6c6bae877748bd87953476c5a2d5f26a52f7a
jvillegas88 <villegas.jonathan88@gmail.com>
2019-09-19 10:37:19 -0300

    Beautify code

--- app/assets/javascripts/demography.js
+++ app/assets/javascripts/demography.js
@@ -22,127 +22,130 @@ Congo.demography.action_dashboards = function(){
     // Creamos el overlay
     Congo.dashboards.action_index.create_overlay_and_filter_card();
 
-      if (county_id != '') {
+    if (county_id != '') {
 
-        // Agregamos filtro Comuna
-        Congo.dashboards.action_index.add_county_filter_item()
+      // Agregamos filtro Comuna
+      Congo.dashboards.action_index.add_county_filter_item()
 
-        data = {
-          county_id: county_id,
-          type_geometry:type_geometry,
-          layer_type: layer_type,
-          style_layer: style_layer
-        };
-      }
+      data = {
+        county_id: county_id,
+        type_geometry:type_geometry,
+        layer_type: layer_type,
+        style_layer: style_layer
+      };
+    }
 
-      $.ajax({
-        type: 'GET',
-        url: '/demography/general.json',
-        datatype: 'json',
-        data: data,
-        beforeSend: function() {
+    $.ajax({
+      type: 'GET',
+      url: '/demography/general.json',
+      datatype: 'json',
+      data: data,
+      beforeSend: function() {
 
-          // Mostramos el spinner y deshabilitamos los botones
-          $("#spinner").show();
-          $('.btn').addClass('disabled')
-          $('.close').prop('disabled', true);
+        // Mostramos el spinner y deshabilitamos los botones
+        $("#spinner").show();
+        $('.btn').addClass('disabled')
+        $('.close').prop('disabled', true);
 
-          // Establece el nombre de la capa en el navbar
-          $('#layer-name').text('Demografía & Gasto');
+        // Establece el nombre de la capa en el navbar
+        $('#layer-name').text('Demografía & Gasto');
 
-          // Mostramos el icono de Puntos Proporcionales correspondiente
-          $("#prop-prv").hide();
-          $("#prop-cbr").hide();
-          $("#prop-em").hide();
+        // Mostramos el icono de Puntos Proporcionales correspondiente
+        $("#prop-prv").hide();
+        $("#prop-cbr").hide();
+        $("#prop-em").hide();
 
-          // Mostramos el icono de Heatmap correspondiente
-          $("#heat-prv").hide();
-          $("#heat-cbr").hide();
-          $("#heat-em-norm-dem").show();
+        // Mostramos el icono de Heatmap correspondiente
+        $("#heat-prv").hide();
+        $("#heat-cbr").hide();
+        $("#heat-em-norm-dem").show();
 
-          // Eliminamos los chart-containter de la capa anterior
-          $(".chart-container").remove();
+        // Eliminamos los chart-containter de la capa anterior
+        $(".chart-container").remove();
 
-          // Mostramos el filtro de la capa y ocultamos los demás
-          $('.filter-building-regulations').hide();
-          $('.filter-transactions').hide();
-          $('.filter-projects').hide();
-          $('.filter-future-projects').hide();
+        // Mostramos el filtro de la capa y ocultamos los demás
+        $('.filter-building-regulations').hide();
+        $('.filter-transactions').hide();
+        $('.filter-projects').hide();
+        $('.filter-future-projects').hide();
 
-          // Eliminamos el time_slider
-          $('#time_slider_item').remove()
+        // Eliminamos el time_slider
+        $('#time_slider_item').remove()
 
-        },
-        success: function(data) {
+      },
+      success: function(data) {
 
 
-          // Ocultamos el spinner y habilitamos los botones
-          $("#spinner").hide();
-          $('.btn').removeClass('disabled')
-          $('.close').prop('disabled', false);
+        // Ocultamos el spinner y habilitamos los botones
+        $("#spinner").hide();
+        $('.btn').removeClass('disabled')
+        $('.close').prop('disabled', false);
 
-          // Separamos la información
-          for (var i = 0; i < data.length; i++) {
+        // Separamos la información
+        for (var i = 0; i < data.length; i++) {
 
-            var reg = data[i];
-            console.log(reg);
-            var title = reg['title'];
+          var reg = data[i];
+          console.log(reg);
+          var title = reg['title'];
 
-            // Creamos el div contenedor
-            var chart_container = document.createElement('div');
-            chart_container.className = 'chart-container card text-light bg-primary';
-            chart_container.id = 'chart-container'+i;
+          // Creamos el div contenedor
+          var chart_container = document.createElement('div');
+          chart_container.className = 'chart-container card text-light bg-primary';
+          chart_container.id = 'chart-container'+i;
 
-            // Creamos el card-header
-            var card_header = document.createElement('div');
-            card_header.className = 'card-header pl-3';
-            card_header.id = 'header'+i;
+          // Creamos el card-header
+          var card_header = document.createElement('div');
+          card_header.className = 'card-header pl-3';
+          card_header.id = 'header'+i;
 
-            // Creamos el collapse
-            var collapse = document.createElement('div');
-            collapse.className = 'collapse show';
-            collapse.id = 'collapse'+i;
+          // Creamos el collapse
+          var collapse = document.createElement('div');
+          collapse.className = 'collapse show';
+          collapse.id = 'collapse'+i;
 
-            // Creamos el card-body
-            var card_body = document.createElement('div');
-            card_body.className = 'card-body';
-            card_body.id = 'body'+i;
+          // Creamos el card-body
+          var card_body = document.createElement('div');
+          card_body.className = 'card-body';
+          card_body.id = 'body'+i;
 
-            // Creamos handle, título y botones
-            var card_handle = '<span class="fas fa-arrows-alt handle border border-dark">'
-            var card_header_title = '<b>'+title+'</b>'
-            var card_min_button = '<button class="close" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'" aria-label="Minimize"><i class="fas fa-window-minimize" style="width: 24px; height: 12px"></i></button>'
-            var card_max_button = '<button class="close" id="card-max-'+i+'" onclick="maxCard('+i+')"><i class="fas fa-window-maximize" style="width: 24px; height: 12px"></i></button>'
+          // Creamos handle, título y botones
+          var card_handle = '<span class="fas fa-arrows-alt handle border border-dark">'
+          var card_header_title = '<b>'+title+'</b>'
+          var card_min_button = '<button class="close" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'" aria-label="Minimize"><i class="fas fa-window-minimize" style="width: 24px; height: 12px"></i></button>'
+          var card_max_button = '<button class="close" id="card-max-'+i+'" onclick="maxCard('+i+')"><i class="fas fa-window-maximize" style="width: 24px; height: 12px"></i></button>'
 
-            // Adjuntamos los elementos
-            $('.overlay').append(chart_container);
-            $('#chart-container'+i).append(card_header, collapse);
-            $('#collapse'+i).append(card_body);
-            $('#header'+i).append(card_handle, card_header_title, card_max_button, card_min_button);
+          // Adjuntamos los elementos
+          $('.overlay').append(chart_container);
+          $('#chart-container'+i).append(card_header, collapse);
+          $('#collapse'+i).append(card_body);
+          $('#header'+i).append(card_handle, card_header_title, card_max_button, card_min_button);
 
-            // Resumen
-            if (title == "Información general") {
+          // Resumen
+          if (title == "Información general") {
 
-              var info = reg['data'];
-              console.log(info);
+            var info = reg['data'];
 
-              // Extraemos y adjuntamos los datos al card-body
-              $.each(info, function(y, z){
-                name = z['name'];
-                count = z['count']
-                count = count.toLocaleString('es-ES')
-                item = name+': '+count+'<br>';
-                $('#body'+i).append(item);
-              })
-
-            }
+            // Extraemos y adjuntamos los datos al card-body
+            $.each(info, function(y, z) {
+              name = z['name'];
+              count = z['count']
+              count = count.toLocaleString('es-ES')
+              item = name + ': ' + count + '<br>';
+              $('#body' + i).append(item);
+            })
 
           }
 
-        }
-      });
+          if (title == "Fuente de datos") {
 
-  }
+
+
+          }
+
+        } // Cierra for
+      } // Cierra success
+    }) // Cierra ajax
+  } // Cierra indicator_demography
   return {
     init: init,
     indicator_demography: indicator_demography
