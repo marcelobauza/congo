986aadf0ef3b922ec4305c121dc225307c3ab638
Sergio <asergiop21@gmail.com>
2019-10-07 23:51:07 -0300

    Fix popup in transactions

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,6 +1,13 @@
 class Admin::DashboardsController < ApplicationController
-  layout 'admin'
 
+  before_action :verify_admin
+  layout 'admin'
+  
   def index
+  end
+  
+  private
+  def verify_admin
+    redirect_to root_url unless current_user.role.name == 'Admin' or current_user.role.name == 'Super admin'
   end
 end
