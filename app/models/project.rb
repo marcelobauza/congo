148c213bff36198f02f4d000b5fbfa9f234ee881
Sergio <asergiop21@gmail.com>
2019-10-18 01:20:20 -0300

    Fix joins in function projects_group_by_count

--- app/models/project.rb
+++ app/models/project.rb
@@ -327,18 +327,21 @@ class Project < ApplicationRecord
   def self.projects_group_by_count(widget, filters, has_color, range)
     @joins = Array.new
 
+      @joins << "INNER JOIN project_instances ON projects.id = project_instances.project_id "
     case widget
     when 'agencies'
-      #@joins << "INNER JOIN agencies ON agencies.id = project_instance_mix_views.agency_id "
+      @joins << "INNER JOIN agency_rols ON projects.id = agency_rols.agency_id "
+      @joins << "INNER JOIN agencies ON agencies.id = .agency_id "
       select = "agency_name, agency_id"
       count = "agency_name"
     when 'project_types'
-      @joins << "INNER JOIN project_types ON project_types.id = project_instance_mix_views.project_type_id "
+      @joins << "INNER JOIN project_types ON project_types.id = projects.project_type_id "
       select = "#{widget}.id, #{widget}.name"
       select += ", #{widget}.color" if has_color
       count = "#{widget}.name"
     when 'project_statuses'
-      @joins << "INNER JOIN project_statuses ON project_statuses.id = project_instance_mix_views.project_status_id "
+      @joins << "INNER JOIN project_instances ON projects.id = project_instances.project_id "
+      @joins << "INNER JOIN project_statuses ON project_statuses.id = project_instances.project_status_id "
       select = "#{widget}.id, #{widget}.name"
       select += ", #{widget}.color" if has_color
       count = "#{widget}.name"
@@ -346,7 +349,7 @@ class Project < ApplicationRecord
     #select = "#{widget}.id, #{widget}.name"
     #select += ", #{widget}.color" if has_color
 
-    ProjectInstanceMixView.select( "#{select}, COUNT(#{count}) as value").
+    Project.select( "#{select}, COUNT(#{count}) as value").
       joins(@joins.uniq.join(" ")).
       where(build_conditions_new(filters, widget, false, range)).
       group(select).
