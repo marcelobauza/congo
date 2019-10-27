f60ecd9caa1349e21ca42f60499f2ab28940651f
Sergio <asergiop21@gmail.com>
2019-10-27 02:10:19 -0300

    Validation selection empty

--- app/views/demography/dashboards.js
+++ app/views/demography/dashboards.js
@@ -1,3 +1,12 @@
-Congo.dashboards.config.layer_type = 'demography_info';
-Congo.dashboards.config.style_layer = 'census_voronoi_gse_zn';
-Congo.map_utils.counties();
+county = Congo.dashboards.config.county_id;
+centerpt = Congo.dashboards.config.centerpt;
+size_box = Congo.dashboards.config.size_box;
+if (county.length > 0  || centerpt != '' || size_box.length >0){
+  Congo.dashboards.config.layer_type = 'demography_info';
+  Congo.dashboards.config.style_layer = 'census_voronoi_gse_zn';
+  Congo.map_utils.counties();
+}else{
+
+  Congo.dashboards.action_index.empty_selection_alert();
+
+}
