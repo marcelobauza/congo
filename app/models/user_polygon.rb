88493e099d073c4f5fb555200e3b1e1e3dd91c87
Sergio <asergiop21@gmail.com>
2019-12-13 15:24:16 -0300

    Change to sym params for  user_polygons

--- app/models/user_polygon.rb
+++ app/models/user_polygon.rb
@@ -3,6 +3,7 @@ class UserPolygon < ApplicationRecord
 
   def self.save_polygons_for_user data
     @wkt =  convert_geometry_as_text data
+    return unless @wkt.nil?
     @wkt.each do |wkt|
       up = UserPolygon.new()
       up.wkt = wkt['wkt']
@@ -13,16 +14,16 @@ class UserPolygon < ApplicationRecord
   end
 
   def self.convert_geometry_as_text data
-    type_geometry = data['type_geometry']
+    type_geometry = data[:type_geometry]
     case type_geometry
     when 'circle' 
       radius = data['radius'].to_f / 1000
       @wkt = ActiveRecord::Base.connection.execute("select ST_ASTEXT(st_buffer(ST_GeomFromText('POINT(#{data[:centerpt]})'), #{radius}, 8 )) as wkt")
     when 'polygon'
-      polygon = JSON.parse(data['wkt'])
+      polygon = JSON.parse(data[:wkt])
       @wkt = ActiveRecord::Base.connection.execute("select st_astext(st_geomfromGeojson('{\"type\":\"Polygon\", \"coordinates\": #{polygon[0]} }')) as wkt")
     else
-      county_id = data['county_id']
+      county_id = data[:county_id]
       county = County.find(county_id)
       @wkt=[]
       county.each do |c|
