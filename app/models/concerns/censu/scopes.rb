fa652b30cf5cf8fd1ece4f6dcf49fb2360856c14
Sergio <asergiop21@gmail.com>
2020-04-20 22:43:48 -0300

    Fix in query verification of region in census

--- app/models/concerns/censu/scopes.rb
+++ app/models/concerns/censu/scopes.rb
@@ -5,7 +5,7 @@ module Censu::Scopes
     def verification_for_region filters
 
       if !filters[:county_id].nil?
-        name_county = County.where(id: filters[:county_id]).pluck(:name).first
+        name_county = County.where(id: filters[:county_id]).pluck(:name)
         conditions = validation_if_name_exists(name_county)
       elsif !filters[:wkt].nil?
         name_counties = County.where(WhereBuilder.build_intersects_condition(filters[:wkt])).pluck(:name)
@@ -21,7 +21,7 @@ module Censu::Scopes
 
       region = %w(Santiago Providencia)
       name.each do |n| 
-          return  'santiago_only= true' if region.include? n
+        return  'santiago_only= true' if region.include? n
       end
       return 'santiago_only= false'
     end
