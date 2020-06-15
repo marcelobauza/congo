ef6c8949c0f704b5492e66050785db5209af5ae9
Sergio <asergiop21@gmail.com>
2020-06-15 18:51:18 -0300

    disable has_paper_trail in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_paper_trail
+  #has_paper_trail
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
