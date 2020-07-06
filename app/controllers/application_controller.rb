bbfac06fe6e6f79b45213736930699cedd96dfac
Sergio <asergiop21@gmail.com>
2020-07-06 16:45:58 -0300

    disable paper trail option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :set_paper_trail_whodunnit
+  #before_action :set_paper_trail_whodunnit
   layout :layout_by_resource
 
 
