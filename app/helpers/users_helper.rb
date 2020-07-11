e066b8ee2f9e6d39439d47249d7b1eda94e0ce11
Sergio <asergiop21@gmail.com>
2020-07-11 10:46:03 -0300

    accumulated download by users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,9 @@ module UsersHelper
       where('created_at::date =?', Date.today).
       sum(layer)
   end
+
+  def accumulated_download_by_user user_id, layer
+    u         = User.find(user_id)
+    downloads = u.downloads_users.sum(layer)
+  end
 end
