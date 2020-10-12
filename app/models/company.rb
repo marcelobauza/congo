0c6ffa062a5b7f363526e454acb136370a592d0a
Sergio <asergiop21@gmail.com>
2020-10-12 17:36:26 -0300

    Disabled validations in company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ApplicationRecord
   has_many :users
 
   include Companies::Validations
+
   def to_s
     name.to_s
   end
--- app/models/concerns/companies/validations.rb
+++ app/models/concerns/companies/validations.rb
@@ -7,10 +7,10 @@ module Companies::Validations
       :transactions_downloads,
       :future_projects_downloads,
       presence: true
-    validates :projects_downloads,
-      :transactions_downloads,
-      :future_projects_downloads,
-      numericality: true,
-      numericality: { only_integer: true }
+    # validates :projects_downloads,
+    #   :transactions_downloads,
+    #   :future_projects_downloads,
+    #   numericality: true,
+    #   numericality: { only_integer: true }
   end
 end
