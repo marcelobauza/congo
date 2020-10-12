1e09cea726cbfbb80b03a8c5f42d9034887a8e80
Sergio <asergiop21@gmail.com>
2020-10-12 17:45:26 -0300

    Disabled validations in company model

--- app/models/concerns/companies/validations.rb
+++ app/models/concerns/companies/validations.rb
@@ -2,11 +2,11 @@ module Companies::Validations
   extend ActiveSupport::Concern
 
   included do
-    validates :name,
-      :projects_downloads,
-      :transactions_downloads,
-      :future_projects_downloads,
-      presence: true
+    # validates :name,
+    #   :projects_downloads,
+    #   :transactions_downloads,
+    #   :future_projects_downloads,
+    #   presence: true
     # validates :projects_downloads,
     #   :transactions_downloads,
     #   :future_projects_downloads,
