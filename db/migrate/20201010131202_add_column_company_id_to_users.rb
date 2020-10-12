b4dc1d84a142e1d7c075607467e41fcf8103d16b
Sergio <asergiop21@gmail.com>
2020-10-12 17:27:48 -0300

    Fix migrations

--- db/migrate/20201010131202_add_column_company_id_to_users.rb
+++ db/migrate/20201010131202_add_column_company_id_to_users.rb
@@ -3,7 +3,13 @@ class AddColumnCompanyIdToUsers < ActiveRecord::Migration[5.2]
     add_reference :users, :company
 
     User.all.each do |u|
-      company = Company.where(name: u.company).first_or_create!
+      company = Company.where(
+        name: u.company,
+        projects_downloads: 0,
+        transactions_downloads: 0,
+        future_projects_downloads: 0
+      ).first_or_create!
+
       u.update_column :company_id, company.id
     end
 
