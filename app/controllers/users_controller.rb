f0071a1ac627b8eb0a290da67bfe8fd3d3135a57
Sergio <asergiop21@gmail.com>
2020-11-18 15:18:41 -0300

    Fix export in download of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   end
 
   def export_csv_downloads_by_company
-    data = DownloadsUser.export_csv_downloads_by_company current_user.company_id
+    data = DownloadsUser.export_csv_downloads_by_company current_user.company_id, current_user
 
     send_file data, :type => 'text/csv', :disposition => "inline", :filename => "Descargar_por_empresa.csv"
   end
