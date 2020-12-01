f1f7f19adac0ac255fafeae40a710063efd4330a
Sergio <asergiop21@gmail.com>
2020-12-01 11:13:23 -0300

    Add popup in boost of transactions

--- app/models/concerns/transactions/popup.rb
+++ app/models/concerns/transactions/popup.rb
@@ -4,7 +4,15 @@ module Transactions::Popup
   module ClassMethods
     def popup params
       @row = Transaction.where(id: params[:id]).first
-      @data = Transaction.where("st_Dwithin(st_geomfromtext('#{@row.the_geom}',4326), the_geom, 15, false)").where(WhereBuilder.build_range_periods_by_bimester_transaction_popup(params[:bimester], params[:year], 6))
+      @data = Transaction.where("st_Dwithin(st_geomfromtext('#{@row.the_geom}',4326), the_geom, 15, false)").boost(params)
+    end
+
+    def boost params
+      if params[:boost] == 'false'
+        where(WhereBuilder.build_range_periods_by_bimester_transaction_popup(params[:bimester], params[:year], 6))
+      else
+        all
+      end
     end
   end
 end
