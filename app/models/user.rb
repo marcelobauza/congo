a8b2d1c1d211a93d19200cb258fb4e374533d145
Sergio <asergiop21@gmail.com>
2020-12-17 11:01:59 -0300

    Add export download limits in admin users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   include Util
+  include Users::Export
   #has_paper_trail
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
