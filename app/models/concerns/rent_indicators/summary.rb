bb5c6ef1f41bacd16ce2cbc5b4a95ede88b9331e
Sergio <asergiop21@gmail.com>
2021-01-19 11:52:11 -0300

    Fix

--- app/models/concerns/rent_indicators/summary.rb
+++ app/models/concerns/rent_indicators/summary.rb
@@ -2,6 +2,7 @@ module RentIndicators::Summary
   extend ActiveSupport::Concern
 
   module ClassMethods
+
     def summary params
       data   = []
       result = []
@@ -24,21 +25,22 @@ module RentIndicators::Summary
         "ST_CONTAINS(
           ST_GEOMFROMTEXT('#{neighborhood.the_geom}',4326), the_geom)"
       ).
-      where(bimester: 1, year: params[:to_year])
+      where(bimester: params[:period], year: params[:to_year])
 
       bots = Bot.where(
         "ST_CONTAINS(
           ST_GEOMFROMTEXT('#{neighborhood.the_geom}',4326), the_geom)"
-      )
+      ).
+      where(bimester: params[:period], year: params[:to_year])
 
       rent_offer = (bots.count / 0.9)
       rent_department = (neighborhood.total_departments.to_i + future_projects.count) * neighborhood.tenure
-      total_vacancy = rent_department / rent_offer
+      total_vacancy = rent_department.to_f / rent_offer.to_f
       avg_u_rent = bots.average(:surface)
       avg_u_sale = transactions.average(:total_surface_building)
       avg_cbr = transactions.average(:calculated_value)
       avg_price_uf = bots.average(:price_uf)
-      avg_price_uf_m2 = bots.average(:price_uf) / avg_u_rent
+      avg_price_uf_m2 = bots.average(:price_uf).to_f / avg_u_rent.to_f
 
       data.push("name": "Total Viviendas", "count": neighborhood.total_houses)
       data.push("name": "Total Departamentos", "count": neighborhood.total_departments)
@@ -54,8 +56,76 @@ module RentIndicators::Summary
       data.push("name": "Precio Oferta Arriendo | UFm2 mensual", "count": avg_price_uf_m2.to_f)
       data.push("name": "PxQ mensual | UF miles", "count": 4)
 
-      result.push("title": "Resumen Bimestre", data: data)
-      result
+      result.push({"title": "Resumen Bimestre", data: data})
+      result.push({"title": "Distribución Programas", "series": distribution_by_mix_types(projects)})
+      result.push({"title": "Superficie", "series": surface(bots)})
+      result.push({"title": "Precio UF mes", "series": price_uf_by_bimester(bots)})
+      result.push({"title": "UFm2 mes", "series": price_uf_by_bimester(bots)})
+      result.push({"title": "Relación Precios | Vacancia", "series": relation_price_by_vacancy(bots)})
+    end
+
+    private
+    def distribution_by_mix_types projects
+      mix_types = projects.group_by { |s| "#{s.bedroom}.#{s.half_bedroom}|#{s.bathroom}"}
+      data      = []
+
+      mix_types.map do |key, mix|
+        data.push("name": key, "count": mix.size)
+      end
+
+      series = [{
+        "label": "Parque",
+        "data": data
+      }]
+    end
+
+    def surface bots
+      avg  = bots.average(:price_uf).to_f
+      data = []
+
+      data.push("name": "6/20", "count": avg.to_f )
+
+      series = [{
+        "label": "Arriendo",
+        "data": data
+      }]
+    end
+
+    def price_uf_by_bimester bots
+      avg  = bots.average(:price_uf).to_f
+      data = []
+
+      data.push("name": "6/20", "count": avg.to_f)
+
+      series = [{
+        "label": "Arriendo",
+        "data": data
+      }]
+
+    end
+
+    def price_ufm2_by_bimester bots
+      avg  = bots.average(:price_uf).to_f
+      data = []
+
+      data.push("name": "6/20", "count": avg.to_f )
+
+      series = [{
+        "label": "Arriendo",
+        "data": data
+      }]
+    end
+
+    def relation_price_by_vacancy bots
+      avg  = bots.average(:price_uf).to_f
+      data = []
+
+      data.push("name": "6/20", "count": avg.to_f )
+
+      series = [{
+        "label": "Arriendo",
+        "data": data
+      }]
     end
   end
 end
--- app/models/rent_indicator.rb
+++ app/models/rent_indicator.rb
@@ -3,16 +3,6 @@ class RentIndicator < ApplicationRecord
   include RentIndicators::Summary
 
   def self.rent_geo filters
-    #county
-   # rent = HomogeneousZone.where("st_intersects(geom, (select the_geom from counties where code = '13101'))").
-
-    #centerpt = filters[:centerpt]
-    #radius = filters[:r].to_f
-    #radius_b = filters[:r].to_f / 100000
-    #pp = "ST_DWithin(ST_GeomFromText('POINT(#{centerpt})', 4326), geom, #{radius},false)"
-    ##pp = "ST_intersects(geom, ST_buffer(st_intersection(geom, ST_GeomFromText('POINT(#{centerpt})', 4326), #{radius}, 'quad_segs=8')))"
-    ##pp_s = "ST_AsEwkt(ST_Difference(ST_buffer(ST_intersection(geom, ST_GeomFromText('POINT(#{centerpt})',4326) ), #{radius_b}, 'quad_segs=8'), geom)) as geom, geocode"
-    #pp_s = "st_asewkt(ST_buffer(ST_GeomFromText('POINT(#{centerpt})', 4326), #{radius_b})) as geom, geocode"
     rent =  Neighborhood.where(
       conditions_by(filters)).
       select(
