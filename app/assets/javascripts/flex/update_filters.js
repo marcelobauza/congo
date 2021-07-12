66aa56ca36d97aececde82880af347028ceef1b7
diegodev9 <diegodev9@gmail.com>
2021-07-12 12:06:58 -0300

    correccion de filter update

--- app/assets/javascripts/flex/update_filters.js
+++ app/assets/javascripts/flex/update_filters.js
@@ -1,10 +1,11 @@
-function update_filters(data) {
-    parsed_data = data;
+function update_filters() {
+    //parsed_data = data;
+    $(".multiselect-native-select option").remove()
 
     $('tr.genTable').remove();
     $(".chartjs-render-monitor").removeAttr('class').removeAttr('style').removeAttr('width').removeAttr('height');
 
-    $("#prop_type").empty()
+    //$("#prop_type").empty()
 
     $(parsed_data['property_types']).each(function () {
         $("#prop_type").append($('<option>').val($(this)[1]).text($(this)[0]));
