ad60acf8541df24f937f9571c4117432f87ca4ed
Jonathan Villegas <villegas.jonathan88@gmail.com>
2021-09-22 17:53:45 -0300

    Cambia leafletImage por html2canvas para la captura del mapa en EM

--- app/assets/javascripts/future_projects.js
+++ app/assets/javascripts/future_projects.js
@@ -107,18 +107,25 @@ future_projects_report_pdf = function() {
     },
     success: function(data) {
 
-      let build_image_map = new Promise((resolve, reject) => {
-        leafletImage(map, function(err, canvas) {
-          var img = document.createElement('img');
-          var dimensions = map.getSize();
-          img.width = dimensions.x;
-          img.height = dimensions.y;
-          img.src = canvas.toDataURL();
-          resolve(img);
-        });
-      });
+      var mapContainer = document.getElementById('map');
+      $('.leaflet-top').addClass('d-none');
+      $('.leaflet-right').addClass('d-none');
+      $('.leaflet-html-legend').addClass('d-none');
 
-      build_image_map.then(function(img) {
+      html2canvas(mapContainer, {
+        useCORS: true,
+      }).then(function(canvas) {
+
+        var img = document.createElement('img');
+        var dimensions = map.getSize();
+        img.width = dimensions.x;
+        img.height = dimensions.y;
+        img.src = canvas.toDataURL();
+
+        // Oculta elementos leaflet para sacar la captura
+        $('.leaflet-top').removeClass('d-none');
+        $('.leaflet-right').removeClass('d-none');
+        $('.leaflet-html-legend').removeClass('d-none');
 
         // Habilitar la interacción con el mapa
         map.dragging.enable();
