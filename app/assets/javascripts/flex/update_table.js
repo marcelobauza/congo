e6dbd1fa88a5e3dac526d8e2668cda1429c853cb
Sergio <asergiop21@gmail.com>
2021-09-22 20:59:20 -0300

    arreglo numericos en ordenar

--- app/assets/javascripts/flex/update_table.js
+++ app/assets/javascripts/flex/update_table.js
@@ -26,14 +26,14 @@ function update_table() {
     clearTable();
 
     $("#sel-box").css('display', 'block');
-    $("#sel-box").on('click', function(){
-        if( $(this).is(':checked') ) {
-            $("#table .form-check-input").each(function(){
-                $(this).prop('checked', true);
-                $(".input-checkbox .form-check-input").closest('td').css('background','#45feed');
+    $("#sel-box").on('click', function() {
+        if($(this).is(':checked')) {
+            $("#table .form-check-input").each(function() {
+              $(this).prop('checked', true);
+              $(".input-checkbox .form-check-input").closest('td').css('background','#45feed');
             });
         } else {
-            $("#table .form-check-input").each(function(){
+          $("#table .form-check-input").each(function() {
                 $(this).prop('checked', false);
                 $(".input-checkbox .form-check-input").closest('td').css('background','#ed36be');
             });
@@ -68,81 +68,63 @@ function update_table() {
 
     var table = $('table');
 
-    $('#utilm2_sort, #e_sort, #uf_sort')
-        .wrapInner('<span title="ordenar esta columna"/>')
-        .each(function(){
+  $('#utilm2_sort, #e_sort, #uf_sort')
+    .wrapInner('<span title="ordenar esta columna"/>')
+    .each(function() {
+      var th      = $(this),
+          thIndex = th.index(),
+          inverse = false;
 
-            var th = $(this),
-                thIndex = th.index(),
-                inverse = false;
-
-            th.click(function(){
-
-                table.find('td.for-order').filter(function(){
-
-                    return $(this).index() === thIndex;
-
-                }).sortElements(function(a, b){
-
-                    if( $.text([a]) == $.text([b]) )
-                        return 0;
-
-                    return $.text([a]) > $.text([b]) ?
-                        inverse ? -1 : 1
-                        : inverse ? 1 : -1;
-
-                }, function(){
-
-                    // parentNode is the element we want to move
-                    return this.parentNode;
-
-                });
-
-                inverse = !inverse;
-
-            });
-
+      th.click(function() {
+        table.find('td.for-order').filter(function() {
+          return $(this).index() === thIndex;
+        }).sortElements(function(a, b) {
+          if( +$.text([a]) == +$.text([b]) )
+            return 0;
+          return +$.text([a]) > +$.text([b]) ?
+            inverse ? -1 : 1
+            : inverse ? 1 : -1;
+        }, function(){
+          // parentNode is the element we want to move
+          return this.parentNode;
         });
+
+        inverse = !inverse;
+      });
+    });
+
     $('#address_sort')
-        .wrapInner('<span title="ordenar esta columna"/>')
-        .each(function(){
+    .wrapInner('<span title="ordenar esta columna"/>')
+    .each(function() {
+      var th    = $(this),
+        thIndex = th.index(),
+        inverse = false;
 
-            var th = $(this),
-                thIndex = th.index(),
-                inverse = false;
+      th.click(function(){
 
-            th.click(function(){
+        table.find('td.for-order').filter(function(){
+          return $(this).index() === thIndex;
+        }).sortElements(function(a, b) {
 
-                table.find('td.for-order').filter(function(){
+          if( $.text([a]).split(/\d\s/).reverse().join(" ") == $.text([b]).split(/\d\s/).reverse().join(" ") )
+            return 0;
 
-                    return $(this).index() === thIndex;
-
-                }).sortElements(function(a, b){
-
-                    if( $.text([a]).split(/\d\s/).reverse().join(" ") == $.text([b]).split(/\d\s/).reverse().join(" ") )
-                        return 0;
-
-                    return $.text([a]).split(/\d\s/).reverse().join(" ") > $.text([b]).split(/\d\s/).reverse().join(" ") ?
-                       inverse ? -1 : 1
-                       : inverse ? 1 : -1;
-
-                }, function(){
-
-                    // parentNode is the element we want to move
-                    return this.parentNode;
-
-                });
-
-                inverse = !inverse;
-
-            });
-
+          return $.text([a]).split(/\d\s/).reverse().join(" ") > $.text([b]).split(/\d\s/).reverse().join(" ") ?
+            inverse ? -1 : 1
+            : inverse ? 1 : -1;
+        }, function(){
+          return this.parentNode;
         });
-    $(".input-checkbox .form-check-input").each(function(){
-        $(this).change(function(){
-            $(this).is(':checked') ? $(this).closest('td').css('background','#45feed') : $(this).closest('td').css('background','#ed36be');
-        });
-    })
+
+        inverse = !inverse;
+      });
+    });
+
+  $(".input-checkbox .form-check-input").each(function(){
+    $(this).change(function(){
+      $(this).is(':checked') ? $(this).closest('td').css('background','#45feed') : $(this).closest('td').css('background','#ed36be');
+    });
+  })
 
   values = rowsCheckTable()
 
