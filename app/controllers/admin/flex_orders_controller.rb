048c98bd25f47b7ffb0ffb77477a691b1a1bda45
Sergio <asergiop21@gmail.com>
2021-10-18 20:38:14 -0300

    Fix flex orders

--- app/controllers/admin/flex_orders_controller.rb
+++ app/controllers/admin/flex_orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::FlexOrdersController < ApplicationController
   layout 'admin'
 
   def index
-    date = params[:query][:date].present? ? params[:query][:date].to_date : Time.now
+    date = (params[:query] && params[:query][:date].present?) ? params[:query][:date].to_date : Time.now
 
     @flex_orders = FlexOrder.where created_at: date.beginning_of_day..date.end_of_day
   end
