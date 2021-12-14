40e8d986486ff4f7c9a15dca6400a501f0f64c96
Sergio <asergiop21@gmail.com>
2021-12-13 21:48:35 -0300

    Fix range date in flex orders

--- app/controllers/admin/flex_orders_controller.rb
+++ app/controllers/admin/flex_orders_controller.rb
@@ -2,8 +2,8 @@ class Admin::FlexOrdersController < ApplicationController
   layout 'admin'
 
   def index
-    from = (params[:query] && params[:query][:from]).to_date || Time.now
-    to   = (params[:query] && params[:query][:to]).to_date || Time.now
+    from = (params[:query] && params[:query][:from].to_date) || Time.now
+    to   = (params[:query] && params[:query][:to].to_date) || Time.now
 
     @flex_orders = FlexOrder.where created_at: from.beginning_of_day..to.end_of_day
   end
