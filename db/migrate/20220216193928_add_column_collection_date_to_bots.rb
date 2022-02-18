00c9699bec5b6690b714ff573b1a2b73c75e103b
Sergio <asergiop21@gmail.com>
2022-02-18 20:26:28 -0300

    Delete migration to collection_date field

--- db/migrate/20220216193928_add_column_collection_date_to_bots.rb
+++ db/migrate/20220216193928_add_column_collection_date_to_bots.rb
@@ -1,9 +1,5 @@
 class AddColumnCollectionDateToBots < ActiveRecord::Migration[5.2]
   def change
     add_column :bots, :collection_date, :date
-
-    Bot.all.each do |bot|
-      bot.update_attribute :collection_date, bot.created_at
-    end
   end
 end
