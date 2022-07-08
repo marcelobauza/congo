3045189520fe95350380b0a11a9370c27d6d48b0
Jonathan Villegas <villegas.jonathan88@gmail.com>
2022-07-08 00:28:27 -0300

    Corrige form y update de user en flex

--- config/routes.rb
+++ config/routes.rb
@@ -140,6 +140,7 @@ Rails.application.routes.draw do
     get 'flex_reports/building_regulation_download' => 'flex_reports#building_regulation_download'
     post 'flex_reports/search_data_for_charts'
     get 'users/account' => 'users#account'
+    put 'users/update' => 'users#update'
     resources :dashboards, only: [:index, :new, :create]
     resources :tenements
     resources :flex_reports, only: [:index, :new, :create, :show]
